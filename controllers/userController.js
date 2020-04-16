2964e32e26bd6822fcb9660c8c64409ae6426949
Nadine Ju <nadine.jura@online.de>
2020-04-16 09:55:00 -0700

    failed login flash message added

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,10 @@ exports.login = function(req, res) {
         });
     }).catch(function(err) {
         req.flash("errors", err);
-        res.redirect("/");
-
+        // manually save session, so page doesn't redirect before flash message is loaded
+        req.session.save(function() {
+            res.redirect("/");
+        });
     });
 };
 
@@ -37,6 +39,6 @@ exports.home = function(req, res) {
     if (req.session.user) {
         res.render("home-dashboard", { username: req.session.user.username });
     } else {
-        res.render("home-guest", {  });
+        res.render("home-guest", { errors: req.flash("errors") });
     }
 };
