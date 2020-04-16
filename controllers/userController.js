b698076400e3e45421c368ebbc97aba41a0da885
Nadine Ju <nadine.jura@online.de>
2020-04-16 10:03:21 -0700

    server side validation error flash messages added

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,17 +28,21 @@ exports.register = function(req, res) {
     let user = new User(req.body);
     user.register();
     if (user.errors.length > 0) {
-        res.send(user.errors);
+        user.errors.forEach(function(err) {
+            req.flash("regErrors", err);
+        });
+        req.session.save(function() {
+            res.redirect("/");
+        });
     } else {
         res.send("Congrats! There are no errors.");
     }
-    res.send('Thanks for trying to register!');
 };
 
 exports.home = function(req, res) {
     if (req.session.user) {
         res.render("home-dashboard", { username: req.session.user.username });
     } else {
-        res.render("home-guest", { errors: req.flash("errors") });
+        res.render("home-guest", { errors: req.flash("errors"), regErrors: req.flash("regErrors") });
     }
 };
