82021266b6c12aa73815459f1a90dbb3d2fdf470
Nadine Ju <nadine.jura@online.de>
2020-10-07 10:51:03 +0200

    post can be deleted

--- router.js
+++ router.js
@@ -15,8 +15,12 @@ router.get('/profile/:username', userController.ifUserExists, userController.pro
 // POST ROUTES
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen);
 router.post('/create-post', userController.mustBeLoggedIn, postController.create);
+
 router.get('/post/:id', postController.viewSingle);
+
 router.get('/post/:id/edit', userController.mustBeLoggedIn, postController.viewEditScreen);
 router.post('/post/:id/edit', userController.mustBeLoggedIn, postController.edit);
+
+router.post('/post/:id/delete', userController.mustBeLoggedIn, postController.delete);
 
 module.exports = router;
